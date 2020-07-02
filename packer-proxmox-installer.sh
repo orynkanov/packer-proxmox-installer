fda1684973272317fdd15e8389c9b393730fba44
orynkanov <orynkanov@gmail.com>
2020-07-02 23:37:56 +0600

    first commit

---
+++ packer-proxmox-installer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dnf install -y wget unzip
+# dnf install -y golang
+# dnf install -y make
+
+cd /opt || exit 1
+
+if [[ ! -f /usr/local/bin/packer ]]; then
+    wget https://releases.hashicorp.com/packer/1.6.0/packer_1.6.0_linux_amd64.zip
+    unzip packer_1.6.0_linux_amd64.zip -d /usr/local/bin
+    # terraform version
+fi
+
+# git clone https://github.com/Telmate/terraform-provider-proxmox.git
+# cd terraform-provider-proxmox || exit 1
+
+# go install github.com/Telmate/terraform-provider-proxmox/cmd/terraform-provider-proxmox
+# go install github.com/Telmate/terraform-provider-proxmox/cmd/terraform-provisioner-proxmox
+# make
+
+# mkdir ~/.terraform.d/plugins
+# cp bin/terraform-provider-proxmox ~/.terraform.d/plugins
+# cp bin/terraform-provisioner-proxmox ~/.terraform.d/plugins
