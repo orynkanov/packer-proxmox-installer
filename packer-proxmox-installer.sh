efe6473c2c6e0ef5d0c42c723ae084465afb65c7
orynkanov <orynkanov@gmail.com>
2020-08-31 21:06:07 +0600

    fix fir different centos version

--- packer-proxmox-installer.sh
+++ packer-proxmox-installer.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-dnf install -y wget unzip
+if [[ ! -f /etc/centos-release ]]; then
+    echo OS not CentOS. This script only for CentOS!
+    exit 1
+fi
+OSVER=$(grep -o '[0-9]' /etc/centos-release | head -n1)
+if [[ $OSVER -eq 7 ]]; then
+    dnf install -y wget unzip
+elif [[ $OSVER -eq 8 ]]; then
+    yum install -y wget unzip
+fi
 
 cd /opt || exit 1
 
